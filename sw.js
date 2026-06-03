9aa9e80743a759dc392de6b7c2faca2f6b21c78e
sabintacom-ui <sabinta.com@gmail.com>
2026-06-03 14:29:13 +0700

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'rizquna-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
